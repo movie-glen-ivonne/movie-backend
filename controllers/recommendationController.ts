72e441e5b103c2bb57279048477b205178da5e8e
Ivonne Ojeda Cuervo <ivonne.ojedacuervo@bahag.com>
2024-12-03 11:37:52 +0100

    Recommendations feature finished

--- controllers/recommendationController.ts
+++ controllers/recommendationController.ts
@@ -10,20 +10,23 @@ const libraryRepository = AppDataSource.getRepository(Library);
 const API_MOVIE_URL = process.env.API_MOVIE_URL;
 const API_MOVIE_KEY = process.env.API_MOVIE_KEY;
 
-export const getRecommendations = async (req: Request, res: Response): Promise<Response| any> => {
+export const getRecommendations = async (req: Request, res: Response): Promise<Response | any> => {
     try {
-        var finalRecommendations: string[] = [];
+        console.log('start')
+        const finalRecommendations: string[] = [];
         const { id } = req.params;
-        const userId = (req as Request & { user: any }).user.id
-        const library = await libraryRepository.findOneBy({ id: Number(id), user : userId });
+        const userId = (req as Request & { user: any }).user.id;
+        const library = await libraryRepository.findOneBy({ id: Number(id), user: userId });
 
         if (library == null) {
-            return res.status(204).json({ message: 'No libraries found' })
+            return res.status(204).json({ message: 'No library found' });
         }
 
         const movies = await getLibraryInfo(library.id); 
-        //const recommendations = await getTop10Movies(movies)
-        const recommendations = [
+        const recommendations = JSON.parse(await getTop10Movies(movies))
+
+        console.log('movies' + movies)
+        /*const recommendations = [
             "Spirited Away",
             "Your Name",
             "Akira",
@@ -34,24 +37,32 @@ export const getRecommendations = async (req: Request, res: Response): Promise<R
             "My Neighbor Totoro",
             "Attack on Titan: Junior High",
             "Naruto Shippuden: The Movie"
-        ]
+        ];*/
 
-        recommendations.forEach(async (movie) => {
-            const info = await getMovieInfo(movie)
-            finalRecommendations.push(info);
+        console.log("sending response")
+
+        const promises = recommendations.map(async (movie: string) => {
+            try {
+                console.log(movie);
+                return await getMovieInfo(movie);
+            } catch (err) {
+                console.error(`Error fetching info for movie: ${movie}`, err);
+                return null;
+            }
         });
-
-        console.log(finalRecommendations);
-
-        return finalRecommendations.length > 0 ? res.status(200).json(finalRecommendations) : res.status(400).json({ message: 'No matching results found' });
-    }
-    catch (err) {
-        console.log(err)
-        return res.status(500).json({ error: 'Internal server error 🔴' })
+        
+        const results = await Promise.all(promises);
+        finalRecommendations.push(...results.filter((result) => result !== null));
+        console.log("sending response")
+        return finalRecommendations.length > 0
+            ? res.status(200).json(finalRecommendations)
+            : res.status(400).json({ message: 'No matching results found' });
+    } catch {
+        return res.status(500).json({ error: 'Internal server error 🔴, try again' });
     }
 };
 
-const getLibraryInfo = async (idLibrary: number): Promise<String[]> => {
+const getLibraryInfo = async (idLibrary: number): Promise<string[]> => {
     const libraryByID = await libraryRepository.findOne({
         where: { id: idLibrary },
         relations: ['movies', 'movies.movie'],
@@ -60,55 +71,47 @@ const getLibraryInfo = async (idLibrary: number): Promise<String[]> => {
     if (!libraryByID) {
         throw new Error("Library not found");
     }
-    const listMovies: string[] = libraryByID.movies.map((movie: LibraryMovie) => movie.movie.original_name);
-    return listMovies; 
+    return libraryByID.movies.map((movie: LibraryMovie) => movie.movie.original_name);
 };
 
-const getMovieInfo = async (movie: String): Promise<string> => {
-
-    const response = await axios.get(API_MOVIE_URL + `/search/multi`, {
-        params: {
-            query: movie
-        },
-        headers: {
-            'Authorization': `Bearer ${API_MOVIE_KEY}`
-        }
+const getMovieInfo = async (movie: string): Promise<any | null> => {
+    const response = await axios.get(API_MOVIE_URL + `/search/multi?append_to_response=videos`, {
+        params: { query: movie },
+        headers: { Authorization: `Bearer ${API_MOVIE_KEY}` },
     });
 
-    const filteredMovie = response.data.results.find((data: { title: string }) => data.title === movie);
+    const filteredMovie = response.data.results.find(
+        (data: { title: string }) => data.title === movie
+    );
 
-    const movieInstance = filteredMovie
-        ? {
-            id: filteredMovie.id,
-            poster_path: filteredMovie.poster_path,
-            first_air_date: filteredMovie.media_type === 'movie' ? filteredMovie.release_date : filteredMovie.first_air_date,
-            original_name: filteredMovie.media_type === 'movie' ? filteredMovie.original_title : filteredMovie.original_name,
-            overview: filteredMovie.overview,
-            vote_average: filteredMovie.vote_average,
-            video_url: await getVideo(filteredMovie.id, filteredMovie.media_type),
-            saved: false
+    if (!filteredMovie) return null;
+
+    const movieInstance = {
+        id: filteredMovie.id,
+        poster_path: filteredMovie.poster_path,
+        first_air_date: filteredMovie.media_type === 'movie' ? filteredMovie.release_date : filteredMovie.first_air_date,
+        original_name: filteredMovie.media_type === 'movie' ? filteredMovie.original_title : filteredMovie.original_name,
+        overview: filteredMovie.overview,
+        vote_average: filteredMovie.vote_average,
+        video_url: await getVideo(filteredMovie.id, filteredMovie.media_type),
+        saved: false,
+    };
+
+    return movieInstance;
+};
+
+const getVideo = async (movieId: number, type: string): Promise<string> => {
+    try {
+        const finalUrl = `${API_MOVIE_URL}/${type}/${movieId}/videos`;
+        const videoResponse = await axios.get(finalUrl, {
+            headers: { Authorization: `Bearer ${API_MOVIE_KEY}` },
+        });
+
+        if (videoResponse.status === 200 && videoResponse.data?.results?.length > 0) {
+            return `https://www.youtube.com/watch?v=${videoResponse.data.results[0].key}`;
         }
-        : "";
-
-        console.log(movieInstance)
-    return movieInstance.toString()
-}
-
-const getVideo = async (movieId: number, type: string) : Promise<String> => {
-    console.log('type ' + type + ' ' + movieId )
-    const finalUrl = `${API_MOVIE_URL}/${type}/${movieId}/videos`;
-    var videoResponse = await axios.get(finalUrl, {
-        params: {
-            id: movieId
-        },
-        headers: {
-            'Authorization': `Bearer ${API_MOVIE_KEY}`
-        }
-    });
-    if (videoResponse.status == 200 && videoResponse.data !== undefined && videoResponse.data.results.length > 0){
-        return `https://www.youtube.com/watch?v=${videoResponse.data.results[0].key}`;
+    } catch {
+        return '';
     }
-    else{
-        return ''
-    }
-}
+    return '';
+};
